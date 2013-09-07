2b4b620dda3ccb84ba4a6f6dbc97bd668a08a04f
Kasra Kyanzadeh <me@kasrak.com>
2013-09-07 11:42:33 -0400

    more ui (progress yay)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 window.app = angular.module('app', []);
 
+app.filter('addr_part', function() {
+    return function(address, type) {
+        if (!address) {
+            return;
+        }
+
+        var comps = address.address_components, comp;
+
+        for (var i = 0; i < comps.length; i++) {
+            comp = comps[i];
+            if (comp.types.indexOf(type) != -1) {
+                return comp.long_name;
+            }
+        }
+    };
+});
+
 app.controller('PlanForm', function($scope, Addresses, Map) {
     $scope.addresses = Addresses;
     $scope.addressInput = null;
     $scope.currentAddress = null;
+
+    $scope.loading = false;
+
+    var $progressBar;
 
     var shouldAutocomplete = true;
 
@@ -24,7 +45,23 @@ app.controller('PlanForm', function($scope, Addresses, Map) {
         Map.setCenter(address.geometry.location.lat, address.geometry.location.lng, 14);
     };
 
+    $scope.startPlanning = function() {
+        $scope.loading = true;
+
+        var progress = 5;
+        var incrementProgress = function() {
+            progress += 10;
+            $progressBar.css('width', progress + '%');
+            if (progress < 100) {
+                window.setTimeout(incrementProgress, 500);
+            }
+        };
+        window.setTimeout(incrementProgress, 500);
+    };
+
     $(function() {
+        $progressBar = $('.loading .progress');
+
         $('input.address').on('click', function() {
             this.select();
         }).on('keydown', function(e) {
